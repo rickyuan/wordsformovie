d1299ed6ebd73ffbbab7e9484f434b452ae78c09
rickyuan <rushyzt@gmail.com>
2011-12-03 13:13:45 +0800

    first commit with omniauth bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordsformovie::Application.config.secret_token = '084fbc9950d1b2e75f910809a0612999bf1e3af0d998bd77f439d68552b10ecc66e83c0c21a416dd2de4326480aea0dc5c6b8e0d7b0d8d504710d0ad39f05531'
